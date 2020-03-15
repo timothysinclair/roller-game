8010b457fe8997a9634d0bead1a52242066cae8f
Tim Sinclair <timbbby@gmail.com>
2020-03-15 19:37:38 +1300

    Added cheats

---
+++ Assets/Scripts/Cheats.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cheats : MonoBehaviour
+{
+    public bool cheatsActive = true;
+
+    public string sceneOne;
+    public string sceneTwo;
+
+    // Single instance control
+    private static Cheats instance;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this) { Destroy(this.gameObject); }
+        else { instance = this; }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        if (!cheatsActive) { return; }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene(sceneOne);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene(sceneTwo);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
