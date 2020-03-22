6e46f7b772565455ea592c1b0ac7007631321643
sebbyling <43594362+sebbyling@users.noreply.github.com>
2020-03-23 09:59:21 +1300

    Billboard Shader

---
+++ Assets/Materials/Shaders/Billboard_SCR.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard_SCR : MonoBehaviour
+{
+    Renderer rend;
+
+    private float blendVal = -2.0f;
+    [SerializeField] private float speed;
+
+    [SerializeField] private float adTime;
+    private float adTimeCur;
+
+    private int ad = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("Roller/Billboard");
+
+        adTimeCur = adTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (adTimeCur > 0)
+        {
+            adTimeCur -= Time.deltaTime;
+        }
+        else
+        {
+            blendVal = Mathf.Sin(Time.fixedTime * Mathf.PI * speed) * 2;
+            rend.material.SetFloat("_Blend", blendVal);
+
+            if (blendVal <= -1.95f)
+            {
+                blendVal = -2.0f;
+                adTimeCur = adTime;
+            }
+
+            if (blendVal >= 1.95f)
+            {
+                blendVal = 12.0f;
+                adTimeCur = adTime;
+            }
+        }
+    }
+}
---
+++ Assets/Materials/Shaders/Billboard_SHA.shader
@@ -0,0 +1,46 @@
+﻿Shader "Roller/Billboard"
+{
+	Properties
+	{
+		_Color("Color", Color) = (1, 1, 1, 1)
+		_MainTex("Texture", 2D) = "white" {}
+		_MainTex2("Texture2", 2D) = "white" {}
+
+		_BlendTex("Blend Texture", 2D) = "white" {}
+		_Blend("Blend Value", Range(-2, 2)) = 0.0
+	}
+		SubShader
+		{
+			Tags { "RenderType" = "Opaque" }
+			LOD 100
+				CGPROGRAM
+				#pragma surface surf Standard fullforwardshadows
+
+				sampler2D _MainTex;
+				sampler2D _MainTex2;
+
+				struct Input
+				{
+					float2 uv_MainTex;
+					float2 uv_MainTex2;
+				};
+
+				fixed4 _Color;
+				half _Blend;
+
+				sampler2D _BlendTex;
+
+				void surf(Input IN, inout SurfaceOutputStandard o)
+				{
+					half blendVal = tex2D(_BlendTex, IN.uv_MainTex).r;
+					half finBlend = (blendVal - _Blend) * _Color;
+
+					fixed4 c = lerp(tex2D(_MainTex, IN.uv_MainTex), tex2D(_MainTex2, IN.uv_MainTex2), finBlend);
+					o.Albedo = c.rgb;
+				}
+
+				ENDCG
+		}
+
+	Fallback "Diffuse"
+}
