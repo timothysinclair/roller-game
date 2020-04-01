a5cb9a9279dfaea274ba1f122a48dd73fc4eebe9
Sebastian <ghsfan3@gmail.com>
2020-04-01 23:04:56 +1300

    UI Particle System
    Had some issues with the Unity's particle system and UI so I wrote my own lol.
    - Added UIParticles folder in 'Scripts'
    - Added 2 scripts
    - Added particle prefab

---
+++ Assets/Scripts/UIParticles/UIParticleEmitter.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIParticleEmitter : MonoBehaviour
+{
+    [Header("Basics")]
+    public Sprite texture;
+    public float duration;
+    public bool looping = true;
+    public float speed;
+    public float life;
+    public float size;
+    public Gradient colour;
+    [SerializeField] private bool randomRotation = false;
+    public float startRotation;
+    public float rotationOverTime;
+    public float gravityModifier;
+
+    [Header("Emission")]
+    [SerializeField] private float rate = 10;    // how many a second
+    private float curRate;
+    [SerializeField] private int burst;
+    [SerializeField] private float burstInterval;
+    private float curBurst;
+    private float burstNum = 0;
+
+    [Header("Private Variables")]
+    [SerializeField] private GameObject basePrefab;
+
+    private void Start()
+    {
+        curRate = 1.0f / rate;
+        curBurst = 1.0f / burstInterval;
+    }
+
+    private void Update()
+    {
+        if (rate > 0)
+        {
+            if (curRate > 0)
+            {
+                curRate -= Time.deltaTime;
+            }
+            else
+            {
+                CreateParticle();
+                curRate = 1.0f / rate;
+            }
+        }
+
+        if (burstInterval > 0)
+        {
+            if (curBurst > 0)
+            {
+                curBurst -= Time.deltaTime;
+            }
+            else
+            {
+                CreateBurst();
+                curBurst = 1.0f / burstInterval;
+            }
+        }
+
+        if (!looping)
+        {
+            if (duration > 0)
+            {
+                duration -= Time.deltaTime;
+            }
+            else
+            {
+                this.enabled = false;
+            }
+        }
+    }
+
+    private void CreateParticle()
+    {
+        GameObject part = (GameObject)Instantiate(basePrefab, this.transform);
+
+        UIParticleScript ps = part.GetComponent<UIParticleScript>();
+
+        ps.texture = texture;
+        ps.speed = speed;
+        ps.life = life;
+        ps.size = size;
+        ps.colour = colour;
+
+        if (randomRotation)
+        {
+            ps.startRotation = Random.Range(0, 360);
+        }
+        else
+        {
+            ps.startRotation = startRotation;
+        }
+        ps.rotationOverTime = rotationOverTime;
+        ps.gravityModifier = gravityModifier;
+
+        //part = null;
+    }
+
+    private void CreateBurst()
+    {
+        for (int i = 0; i < burst; i++)
+        {
+            GameObject part = (GameObject)Instantiate(basePrefab, this.transform);
+
+            UIParticleScript ps = part.GetComponent<UIParticleScript>();
+
+            ps.texture = texture;
+            ps.speed = speed;
+            ps.life = life;
+            ps.size = size;
+            ps.colour = colour;
+
+            if (randomRotation)
+            {
+                ps.startRotation = Random.Range(0, 360);
+            }
+            else
+            {
+                ps.startRotation = startRotation;
+            }
+            ps.rotationOverTime = rotationOverTime;
+            ps.gravityModifier = gravityModifier;
+        }
+    }
+}
---
+++ Assets/Scripts/UIParticles/UIParticleScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIParticleScript : MonoBehaviour
+{
+    public Sprite texture;
+    public float speed;
+    public float life;
+    public float size;
+    public Gradient colour;
+    public float startRotation;
+    public float rotationOverTime;
+    public float gravityModifier;
+
+    Image image;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+        rb = GetComponent<Rigidbody2D>();
+
+        image.sprite = texture;
+        image.color = colour.Evaluate(0.0f);
+
+        transform.rotation = Quaternion.Euler(0, 0, startRotation);
+
+        rb.AddForce(transform.up * (speed * 5), ForceMode2D.Impulse);
+        rb.gravityScale = gravityModifier;
+
+        this.transform.localScale = new Vector3(size, size, size);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (life > 0)
+        {
+            life -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (rotationOverTime > 0)
+        {
+            this.transform.Rotate(0, 0, rotationOverTime);
+        }
+
+        image.color = colour.Evaluate(1.0f / life);
+    }
+}
